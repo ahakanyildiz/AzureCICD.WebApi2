e14d2ec96d550fd986736176141131855cf39435
HAKAN <teknomanihah@gmail.com>
2024-08-01 14:34:12 +0300

    Add project files.

---
+++ Todos.WebApi2/Program.cs
@@ -0,0 +1,16 @@
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+
+WebApplication app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/getall", () => Results.Ok(new List<string>()
+{
+    "Example1",
+    "Example2"
+}));
+
+app.Run();
